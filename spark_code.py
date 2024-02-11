5281f8b7d6e05c10c34b570b7071c149709d77c7
Aman Sharma <amanshar@buffalo.edu>
2024-02-11 16:59:45 -0500

    Update spark_code.py

--- spark_code.py
+++ spark_code.py
@@ -1,19 +1,20 @@
 from pyspark.sql import SparkSession
+import os
 
 # Initialize SparkSession
 spark = SparkSession.builder \
     .appName("Read and Write S3") \
     .getOrCreate()
 
-# Define S3 input and output paths
-input_path = "{SPARK_INPUT_PATH}"
-output_path = "{SPARK_OUTPUT_PATH}"
+# Define S3 input and output paths from environment variables
+input_path = os.environ.get("SPARK_INPUT_PATH")
+output_path = os.environ.get("SPARK_OUTPUT_PATH")
 
 # Read file from S3
 df = spark.read.csv(input_path, header=True, inferSchema=True)
 
 # Perform processing (example: convert column to uppercase)
-df_processed = df.withColumn("processed_column", df["firstname"].cast("string").upper())
+df_processed = df.withColumn("processed_column", df["column_to_process"].cast("string").upper())
 
 # Write processed data back to S3
 df_processed.write \
